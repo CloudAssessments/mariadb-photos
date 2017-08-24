dccea0f5982ef5f3ee80df381ce2290206633a35
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-08-24 10:38:02 -0700

    [Finishes #150487348] Listener - Upload Image Blob to MariaDB

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ File System Photo Upload Service
 ===================
 
 PO: Anthony James
+
+## Installation
+
+### MariaDB
+  1. `brew install mariadb`
+  1. `brew services start mariadb`
+
+## Redis
+  1. `brew install redis`
+  1. `brew services start redis`
--- src/app.js
+++ src/app.js
@@ -11,10 +11,50 @@
   limitations under the License.
 */
 const express = require('express');
+const multer = require('multer');
+const mysql2 = require('mysql2');
+const path = require('path');
+const redis = require('redis');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// initialize mysql connection
+const mysql = mysql2.createConnection({
+  host: process.env.MARIA_HOST,
+  port: process.env.MARIA_PORT,
+  user: process.env.MARIA_USER,
+  password: process.env.MARIA_PASSWORD,
+  database: process.env.MARIA_DATABASE || 'photo_demo',
+});
+
+// initialize redis pub/sub for image upload work
+const redisServer = {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  url: process.env.REDIS_URL,
+  password: process.env.REDIS_PASSWORD,
+};
+const pub = redis.createClient(redisServer);
+const sub = redis.createClient(redisServer);
+sub.on('message', require('./listeners/upload')(mysql));
+
+sub.on('error', err => console.error('REDIS_ERROR: ', err));
+sub.subscribe('upload');
+
 // Routes: Homepage
-app.get('/', (req, res) => res.send('Welcome to the mariadb-photos app!'));
+app.get('/', require('./middleware/homepage'));
+
+// Routes: Upload Photo
+app.post(
+  '/photo',
+  multer().single('uploadedImage'),
+  require('./middleware/multipartToImage'),
+  require('./middleware/publishUpload')(pub)
+);
 
 module.exports = app;
