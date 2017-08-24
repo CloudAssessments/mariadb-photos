df484d0f65f803e811eb89be24f4726d470ae98e
Jonathan Lee <jonathanlee@Jon-Lees-MacBook-Pro.local>
2017-08-24 15:43:03 -0700

    Default MariaDB User to 'root'

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const mysql = mysql2.createConnection({
   host: process.env.MARIA_HOST,
   port: process.env.MARIA_PORT,
-  user: process.env.MARIA_USER,
+  user: process.env.MARIA_USER || 'root',
   password: process.env.MARIA_PASSWORD,
   database: process.env.MARIA_DATABASE || 'photo_demo',
 });
